bb29573f2d22be621b923fb37016263fef2f41e2
moxiangqiangu <moxiangqiangu@noreply.gitcode.com>
2025-09-05 22:22:49 +0800

    first commit

---
+++ tutorial-mod-template-1.20.1/src/main/java/com/windy/modid/TutorialModClient.java
@@ -0,0 +1,10 @@
+package com.windy.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
